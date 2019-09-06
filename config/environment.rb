4c9cb4bf5c6d9781d3925729819f7638f3021e1d
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2019-09-06 15:50:09 -0400

    fixing sg error

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'MikeHartley6',
-  :password => 'foobar661',
+  :user_name => Rails.application.credentials.SENDGRID_USERNAME,
+  :password => Rails.application.credentials.SENDGRID_PASSWORD,
   :domain => 'https://suspicious-mahavira-fd689f.netlify.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,

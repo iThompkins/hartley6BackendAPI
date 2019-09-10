f274bcf71c08a3a99c05013a21d5b682e005b7e2
Isaiah Thompkins <iThompkins@iThompkinss-Air.frontier.com>
2019-09-10 19:38:22 -0400

    updating backend

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => Rails.application.credentials.SENDGRID_USERNAME,
-  :password => Rails.application.credentials.SENDGRID_PASSWORD,
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+
 

e50956ab27a355264e462ff233a19bb028b1746c
Isaiah Thompkins <isaiah_thompkins@alumni.brown.edu>
2019-10-25 15:30:51 -0400

    fix

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,17 +4,15 @@ module V1
 
     # POST /v1/login
     def create
-      if User.find_by(email: params[:email])
-        @user = User.find_for_database_authentication(email: params[:email])
-        return invalid_login_attempt unless @user
+      @user = User.find_for_database_authentication(email: params[:username])
+      return invalid_login_attempt unless @user
 
-        if @user.valid_password?(params[:password])
-          sign_in :user, @user
-          render json: @user, serializer: SessionSerializer, root: nil
-          puts json: @user
-        else
-          invalid_login_attempt
-        end
+      if @user.valid_password?(params[:password])
+        sign_in :user, @user
+        render json: @user, serializer: SessionSerializer, root: nil
+        puts json: @user
+      else
+        invalid_login_attempt
       end
     end
 

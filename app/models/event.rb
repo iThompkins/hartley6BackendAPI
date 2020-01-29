9a8dd4de5866516d56516bf441306154bdea3df4
Isaiah Thompkins <isaiah_thompkins@alumni.brown.edu>
2020-01-29 09:19:24 -0500

    adding validations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
-	validates :title, presence: true
-	validates :host, presence: true
-	validates :time, presence: true
+	validates :title, presence: true, length: { maximum: 25 }
+	validates :host, presence: true, length: { maximum: 25 }
+	validates :time, presence: true, length: { maximum: 25 }
+	validates :location, presence: true, length: { maximum: 25 }
 	validates :availability, presence: true
 
 	belongs_to :user
